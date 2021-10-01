01067ac7d5d4418787e0caaa8b3ebfc2e54510c8
thatguyish <ishmell.ramirez@gmail.com>
2021-10-01 16:13:10 -0400

    update file

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 
 public abstract class Product {
+    public String getName() {
+        return name;
+    }
+
     private String name;
+
+    public Double getPrice() {
+        return price;
+    }
+
     private Double price;
     protected ExpirationDate expirationDate;
     public double DISCOUNT_RATE = .10;
@@ -19,10 +28,13 @@ public abstract class Product {
     }
 
     public static void printOrder(Product[] products){
+        double orderTotal = 0d;
         for (Product product:
              products) {
             System.out.println(String.format("Product: %s, Price: %.2f",product.name , product.price));
+            orderTotal+=product.getPrice();
         }
+        System.out.println(String.format("You're total is %.2f",orderTotal));
     }
     public static void printConsumptionQty(ArrayList<Consumable> consumables){
         for (Consumable consumable:
